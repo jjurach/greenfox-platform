ec41a327935b22a347f1c747980b6cb569750386
James Jurach <james.jurach@gmail.com>
2016-02-22 23:34:54 -0600

    read texasparagus cache

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,6 +2,8 @@
 scriptdir=$(cd $(dirname $0); pwd)
 : ${basedir:=$(cd $(dirname $scriptdir); pwd)}
 
+cache_url=http://texasparagus.com/software-93ccbd71.tar
+
 set -e
 
 # initialize a remote host: transfer files and run setup
@@ -14,8 +16,16 @@ if ! ssh $host test -d /greenfox; then
   ssh $host sudo mkdir -p /greenfox/software /greenfox/scripts
   ssh $host sudo chown -R ubuntu /greenfox
 fi
-rsync -av $basedir/software/ $host:/greenfox/software/
+
 rsync -av $basedir/scripts/ $host:/greenfox/scripts/
+
+# attempt to fetch this content from an Internet cache
+if ! ssh $host "ls -l /greenfox/software/jdk*"; then
+  echo downloading from texasparagus ...
+  ssh $host "curl -s $cache_url | tar xCvf /greenfox - || true"
+fi
+
+rsync -av $basedir/software/ $host:/greenfox/software/
 
 ssh $host /greenfox/scripts/setup_jdk.sh
 
